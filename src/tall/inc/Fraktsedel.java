9dfbd486bc1ce03811397f38d1980d951a4e62b3
Oliver Timmerman <96236710+OliverTimmerman@users.noreply.github.com>
2022-04-04 15:55:34 +0200

    pdfWriter Zip

---
+++ src/tall/inc/Fraktsedel.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package tall.inc;
+
+import java.io.OutputStream;
+import javax.swing.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+
+
+
+/**
+ *
+ * @author timme
+ */
+public class Fraktsedel {
+    OutputStream outPut;
+    Document document;
+    PdfWriter writer;
+}
