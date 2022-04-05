52ce1902857dc476e7ac3331d8fb6f48c353c79c
viktorhg <viktorhg@outlook.com>
2022-04-05 10:18:00 +0200

    Tog bort syntaxfel samt skapade Jframe för SkapaNyOrder
    Tog bort syntaxerror som fanns i fraktsedelsklassen så man kan bygga.

--- src/tall/inc/Fraktsedel.java
+++ src/tall/inc/Fraktsedel.java
@@ -7,8 +7,6 @@ package tall.inc;
 
 import java.io.OutputStream;
 import javax.swing.text.Document;
-import com.itextpdf.text.pdf.PdfWriter;
-
 
 
 /**
@@ -18,5 +16,4 @@ import com.itextpdf.text.pdf.PdfWriter;
 public class Fraktsedel {
     OutputStream outPut;
     Document document;
-    PdfWriter writer;
 }
