1576acc3b4faeff3662c71ea41700ec2a1e0bca1
nsagstrom <n.sagstrom@gmail.com>
2022-04-12 12:36:26 +0200

    fix

--- src/tall/inc/Startsida.java
+++ src/tall/inc/Startsida.java
@@ -99,7 +99,7 @@ public class Startsida extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Färdigställ order");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
